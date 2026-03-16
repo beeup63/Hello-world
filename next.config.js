4f1b5762465f75f1ea05f024ce19a297e768866c
beeup63 <teracas63@gmail.com>
2026-03-16 12:42:10 +0900

    fix: add basePath for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Hello-world',
   // Next.js 13 needs this for some static exports or images if not handled
   images: {
     unoptimized: true,
